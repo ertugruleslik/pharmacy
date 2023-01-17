8ccb4b11832b56a209636c2e67ad512343ffa9dd
Ertuğrul Eşlik <ertugruleslik@gmail.com>
2023-01-17 22:57:01 +0300

    Cross

--- src/main/java/com/pharmacy/pharmacyapi/controller/LocationController.java
+++ src/main/java/com/pharmacy/pharmacyapi/controller/LocationController.java
@@ -2,6 +2,7 @@ package com.pharmacy.pharmacyapi.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.pharmacy.pharmacyapi.service.LocationService;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 public class LocationController {
--- src/main/java/com/pharmacy/pharmacyapi/controller/PharmacyController.java
+++ src/main/java/com/pharmacy/pharmacyapi/controller/PharmacyController.java
@@ -2,6 +2,7 @@ package com.pharmacy.pharmacyapi.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.pharmacy.pharmacyapi.service.PharmacyService;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 public class PharmacyController {
